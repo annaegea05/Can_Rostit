4f4f83d76b616b0ef2059ab85f72bc96a86f780b
Anna Egea Capdevila <annaegea5@gmail.com>
2026-09-19 21:03:01 +0200

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const PRODUCTES = [
   { nom: "Mig pollastre", preu: 8.00, img: "img/Mig_pollastre.jpeg", categoria: "A l'ast" },
 
   // Entrants i Acompanyaments
-  { nom: "Empanada de bonito", preu: 4.50, img: "img/empanada_bonito.jpg", categoria: "Entrants i Acompanyaments" },
-  { nom: "Ensaladilla russa", preu: 7.50, img: "img/ensaladilla.jpeg", categoria: "Entrants i Acompanyaments" },
+  { nom: "Empanada de bonito (1 ud)", preu: 2.00, img: "img/empanadilla.png", categoria: "Entrants i Acompanyaments" },
+  { nom: "Ensaladilla de pollastre", preu: 7.50, img: "img/ensaladilla.jpeg", categoria: "Entrants i Acompanyaments" },
   //{ nom: "Amanida de pebrot escalivat amb allada d'all i oli", preu: 7.50, img: "img/Pimientos-asados-al-ajillo.jpeg", categoria: "Entrants i Acompanyaments" },
   //{ nom: "Crema de carbassa", preu: 4.50, img: "img/image.png", categoria: "Entrants i Acompanyaments" },
   { nom: "Escalivada", preu: 7.50, img: "img/Escalivada.jpeg", categoria: "Entrants i Acompanyaments" },
@@ -18,11 +18,12 @@ const PRODUCTES = [
   //{ nom: "Brao de porc en salsa", preu: 8.50, img: "img/brao.webp", categoria: "Plats Cuinats" },
   //{ nom: "Calamars farcits de mar i muntanya", preu: 6.50, img: "img/calamares-mar-montaña.jpg", categoria: "Plats Cuinats" },
   //{ nom: "Rap a la marinera amb cloïsses", preu: 9.00, img: "img/rap_a_la_marinera_amb_cloisses.jpg", categoria: "Plats Cuinats" },
+  { nom: "Fricandó amb bolets", preu: 8.50, img: "img/Fricandó amb bolets.jpeg", categoria: "Plats Cuinats" },
   { nom: "Albòndigues en salsa espanyola", preu: 6.50, img: "img/mandonguilles.jpg", categoria: "Plats Cuinats" },
-  { nom: "Sardines en escabetx", preu: 6.50, img: "img/sardeines.jpeg", categoria: "Plats Cuinats" },
+  { nom: "Sardines en escabetx (1 ud)", preu: 2.00, img: "img/sardeines.jpeg", categoria: "Plats Cuinats" },
   
   // Croquetes i patates
-  //{ nom: "1 Patata al caliu", preu: 0.50, img: "img/caliu.jpeg", categoria: "Croquetes i Patates" },
+  { nom: "1 Patata al caliu", preu: 0.50, img: "img/caliu.jpeg", categoria: "Croquetes i Patates" },
   { nom: "1 Croqueta de pollastre rostit", preu: 1.50, img: "img/Croqueta de pollastre rostit.jpeg", categoria: "Croquetes i Patates" },
   { nom: "1 Croqueta de pernil ibèric", preu: 2.00, img: "img/Croqueta de pernil iberic.jpeg", categoria: "Croquetes i Patates" },
   { nom: "1 Croqueta de gamba", preu: 2.00, img: "img/Croqueta de pollastre rostit.jpeg", categoria: "Croquetes i Patates" }, 
